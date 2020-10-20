63beeb64f4b521ac0ade63d9a1a08324041c4471
andreirhamni09 <andreirhamni09@gmail.com>
2020-10-20 09:42:49 +0700

    Entah lah

--- pages/layout/footer.php
+++ pages/layout/footer.php
@@ -2,7 +2,7 @@
     <div class="float-right d-none d-sm-block">
         <b></b>
     </div>
-    <strong><a href="#">MobilePro</a>.</strong> 2020
+    <strong><a href="#">MobilePro</a>.</strong> Mana kutahu
 </footer>
 </div>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
